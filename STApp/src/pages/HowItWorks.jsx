688a3cb5be5144fbacdf6a1a65ced460fe946b17
benimilululu <jovanovik.martin2@yahoo.com>
2024-04-20 11:14:04 +0200

    how it work dec

--- STApp/src/pages/HowItWorks.jsx
+++ STApp/src/pages/HowItWorks.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default function HowItWorks() {
+  const welcomeText = `Welcome to our app, where fashion meets convenience! With our user-friendly platform, you can effortlessly buy and sell a wide range of stylish clothing, shoes, and accessories. Whether you're looking to refresh your wardrobe or declutter your closet, our app offers a seamless experience for both buyers and sellers.`;
+
+  const text1 = `Browse through our curated collection of trendy clothes and shoes from various brands and designers, or showcase your own items to a community of fashion enthusiasts. From vintage finds to the latest fashion trends, our app caters to every style and budget.`;
+
+  const text2 = `With secure transactions and easy-to-use features, buying and selling on our platform is a breeze. Join our vibrant community today and discover endless possibilities to express your unique style while finding new homes for your pre-loved fashion pieces. Happy shopping and selling!`;
+
+  const textStyle = `mt-10 p-4`;
+
   return (
     <div className='h-screen'>
       <Header />
-      <div className='text-center pt-10 text-2xl text-white border-2 rounded-lg m-4 p-3 pb-10'>
-        <p className='text-3xl'>How it works?</p>
-        <p className='mt-10'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+      <div className='text-center pt-10 text-2xl md:w-4/6 m-auto text-white border-2 rounded-lg p-3 pb-10'>
+        <p className='text-3xl border-b-4 w-fit m-auto'>How it works ?</p>
+        <p className={textStyle}>{welcomeText}</p>
+        <p className={textStyle}>{text1}</p>
+        <p className={textStyle}>{text2}</p>
       </div>
     </div>
   );
